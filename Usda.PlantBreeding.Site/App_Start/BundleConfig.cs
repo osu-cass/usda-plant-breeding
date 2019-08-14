8611edc115a6312e9b1e311df4c63583001003d8
swanscol <swanscol@CASS-DEV26>
2019-08-14 14:59:09 -0700

    Moved dependecy files to gitignored Lib folders

--- Usda.PlantBreeding.Site/App_Start/BundleConfig.cs
+++ Usda.PlantBreeding.Site/App_Start/BundleConfig.cs
@@ -10,21 +10,21 @@ namespace Usda.PlantBreeding.Site
         {
             bundles.Add(new ScriptBundle("~/bundles/noty")
                 .Include(
-                        "~/Scripts/noty/jquery.noty.js",
-                        "~/Scripts/noty/top.js",
-                        "~/Scripts/noty/center.js",
-                        "~/Scripts/noty/default.js",
-                        "~/Scripts/noty/bootstrap.js",
-                        "~/Scripts/noty/bootstrap-multiselect.js"));
+                        "~/Scripts/Lib/noty/jquery.noty.js",
+                        "~/Scripts/Lib/noty/top.js",
+                        "~/Scripts/Lib/noty/center.js",
+                        "~/Scripts/Lib/noty/default.js",
+                        "~/Scripts/Lib/noty/bootstrap.js",
+                        "~/Scripts/Lib/noty/bootstrap-multiselect.js"));
             bundles.Add(new ScriptBundle("~/bundles/jquery-ajax").Include(
-                        "~/Scripts/jquery-ajax/jquery.unobtrusive-ajax.js"));
+                        "~/Scripts/Lib/jquery-ajax/jquery.unobtrusive-ajax.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery/jquery.js",
-                        "~/Scripts/jquery/sizzle.js"));
+                        "~/Scripts/Lib/jquery/jquery.js",
+                        "~/Scripts/Lib/jquery/sizzle.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery-validation/jquery.validate*"));
+                        "~/Scripts/Lib/jquery-validation/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -32,13 +32,13 @@ namespace Usda.PlantBreeding.Site
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap/bootstrap.js",
-                      "~/Scripts/bootstrap/respond.js"));
+                      "~/Scripts/Lib/bootstrap/bootstrap.js",
+                      "~/Scripts/Lib/bootstrap/respond.js"));
             bundles.Add(new ScriptBundle("~/bundles/search").Include(
                      "~/Scripts/PlantBreedingSearch.js",
                      "~/Scripts/autocomplete/auto-complete.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").IncludeDirectory("~/Scripts/jquery-ui", "*.js", true));
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").IncludeDirectory("~/Scripts/Lib/jquery-ui", "*.js", true));
 
             /*.Include(
             "~/node_modules/jquery-ui/ui/*.js",
@@ -46,7 +46,7 @@ namespace Usda.PlantBreeding.Site
             "~/node_modules/jquery-ui/ui/widgets/*.js"));*/
 
             bundles.Add(new ScriptBundle("~/bundles/offlinejs").Include(
-            "~/Scripts/offline-js/offline.min.js"));
+            "~/Scripts/Lib/offline-js/offline.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/modalform").Include(
                         "~/Scripts/modalform.js"));
@@ -55,60 +55,60 @@ namespace Usda.PlantBreeding.Site
                       "~/Content/site.css"));
 
             bundles.Add(new StyleBundle("~/Content/bootstrap/css").Include(
-                "~/Stylesheets/bootstrap/bootstrap.min.css"));
+                "~/Stylesheets/Lib/bootstrap/bootstrap.min.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-              "~/Stylesheets/base/core.css",
-              "~/Stylesheets/base/resizable.css",
-              "~/Stylesheets/base/selectable.css",
-              "~/Stylesheets/base/accordion.css",
-              "~/Stylesheets/base/autocomplete.css",
-              "~/Stylesheets/base/button.css",
-              "~/Stylesheets/base/dialog.css",
-              "~/Stylesheets/base/slider.css",
-              "~/Stylesheets/base/tabs.css",
-              "~/Stylesheets/base/datepicker.css",
-              "~/Stylesheets/base/progressbar.css",
-              "~/Stylesheets/base/theme.css",
-              "~/Stylesheets/base/auto-complete.css"));
+              "~/Stylesheets/Lib/base/core.css",
+              "~/Stylesheets/Lib/base/resizable.css",
+              "~/Stylesheets/Lib/base/selectable.css",
+              "~/Stylesheets/Lib/base/accordion.css",
+              "~/Stylesheets/Lib/base/autocomplete.css",
+              "~/Stylesheets/Lib/base/button.css",
+              "~/Stylesheets/Lib/base/dialog.css",
+              "~/Stylesheets/Lib/base/slider.css",
+              "~/Stylesheets/Lib/base/tabs.css",
+              "~/Stylesheets/Lib/base/datepicker.css",
+              "~/Stylesheets/Lib/base/progressbar.css",
+              "~/Stylesheets/Lib/base/theme.css",
+              "~/Stylesheets/Lib/base/auto-complete.css"));
 
 
             bundles.Add(new StyleBundle("~/Content/offlinecss").Include(
-              "~/Stylesheets/offline/offline-theme-slide.css",
-              "~/Stylesheets/offline/offline-language-english.css"));
+              "~/Stylesheets/Lib/offline/offline-theme-slide.css",
+              "~/Stylesheets/Lib/offline/offline-language-english.css"));
 
             bundles.Add(new StyleBundle("~/Content/offlineindicatorcss").Include(
-              "~/Stylesheets/offline/offline-theme-slide-indicator.css",
-              "~/Stylesheets/offline/offline-language-english-indicator.css"));
+              "~/Stylesheets/Lib/offline/offline-theme-slide-indicator.css",
+              "~/Stylesheets/Lib/offline/offline-language-english-indicator.css"));
 
 
             /*bundles.Add(new StyleBundle("~/Content/tablecss").Include(
                     "~/sorter/style.css"
                     ));*/
             bundles.Add(new ScriptBundle("~/bundles/table").Include(
-                    "~/Scripts/table/jquery.floatThead.min.js",
-                    "~/Scripts/table/jquery.tablesorter.min.js"
+                    "~/Scripts/Lib/table/jquery.floatThead.min.js",
+                    "~/Scripts/Lib/table/jquery.tablesorter.min.js"
                     ));
             bundles.Add(new ScriptBundle("~/bundles/datatable").Include(
-                 "~/Scripts/datatable/jquery.dataTables.min.js",
-                  "~/Scripts/datatable/dataTables.bootstrap.min.js",
+                 "~/Scripts/Lib/datatable/jquery.dataTables.min.js",
+                  "~/Scripts/Lib/datatable/dataTables.bootstrap.min.js",
                  "~/Scripts/datatable/dataTables.fixedColumns.min.js",
-                 "~/Scripts/datatable/dataTables.fixedHeader.min.js",
-                 "~/Scripts/dataTablesExtensions.js"
+                 "~/Scripts/Lib/datatable/dataTables.fixedHeader.min.js",
+                 "~/Scripts/Lib/dataTablesExtensions.js"
 
 
 
              ));
             bundles.Add(new StyleBundle("~/Content/datatablecss").Include(
-                           "~/Stylesheets/datatable/dataTables.bootstrap.min.css",
-                  "~/Stylesheets/datatable/fixedColumns.bootstrap.min.css",
-                 "~/Stylesheets/datatable/fixedHeader.bootstrap.min.css"
+                           "~/Stylesheets/Lib/datatable/dataTables.bootstrap.min.css",
+                  "~/Stylesheets/Lib/datatable/fixedColumns.bootstrap.min.css",
+                 "~/Stylesheets/Lib/datatable/fixedHeader.bootstrap.min.css"
                    //"~/Content/DataTables-1.10.12/extensions/Responsive/css/responsive.bootstrap.min.css"
 
 
                    ));
             bundles.Add(new StyleBundle("~/Content/font-awesome").Include(
-                    "~/Stylesheets/fontawesome/font-awesome.min.css"
+                    "~/Stylesheets/Lib/fontawesome/font-awesome.min.css"
                     ));
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
--- Usda.PlantBreeding.Site/Gruntfile.js
+++ Usda.PlantBreeding.Site/Gruntfile.js
@@ -1,4 +1,4 @@
-/// <binding BeforeBuild='webpack:prod' />
+/// <binding BeforeBuild='webpack:prod, copy' />
 'use strict';
 const webpackConfig = require('./webpack.config');
 
@@ -55,201 +55,201 @@ module.exports = function (grunt) {
             noty: {
                 files: [
                     {
-                        src: "node_modules/noty/js/noty/jquery.noty.js", dest: "Scripts/noty/jquery.noty.js"
+                        src: "node_modules/noty/js/noty/jquery.noty.js", dest: "Scripts/Lib/noty/jquery.noty.js"
                     },
                     {
-                        src: "node_modules/noty/js/noty/layouts/top.js", dest: "Scripts/noty/top.js"
+                        src: "node_modules/noty/js/noty/layouts/top.js", dest: "Scripts/Lib/noty/top.js"
                     },
                     {
-                        src: "node_modules/noty/js/noty/layouts/center.js", dest: "Scripts/noty/center.js"
+                        src: "node_modules/noty/js/noty/layouts/center.js", dest: "Scripts/Lib/noty/center.js"
                     },
                     {
-                        src: "node_modules/noty/js/noty/themes/default.js", dest: "Scripts/noty/default.js"
+                        src: "node_modules/noty/js/noty/themes/default.js", dest: "Scripts/Lib/noty/default.js"
                     },
                     {
-                        src: "node_modules/noty/js/noty/themes/bootstrap.js", dest: "Scripts/noty/bootstrap.js"
+                        src: "node_modules/noty/js/noty/themes/bootstrap.js", dest: "Scripts/Lib/noty/bootstrap.js"
                     },
                     {
-                        src: "node_modules/bootstrap-multiselect/dist/js/bootstrap-multiselect.js", dest: "Scripts/noty/bootstrap-multiselect.js"
+                        src: "node_modules/bootstrap-multiselect/dist/js/bootstrap-multiselect.js", dest: "Scripts/Lib/noty/bootstrap-multiselect.js"
                     }
                 ]
             },
             jqueryajax: {
                 files: [
                     {
-                        src: "node_modules/jquery-ajax-unobtrusive/dist/jquery.unobtrusive-ajax.js", dest: "Scripts/jquery-ajax/jquery.unobtrusive-ajax.js"
+                        src: "node_modules/jquery-ajax-unobtrusive/dist/jquery.unobtrusive-ajax.js", dest: "Scripts/Lib/jquery-ajax/jquery.unobtrusive-ajax.js"
                     }
                 ]
             },
             jquery: {
                 files: [
                     {
-                        src: "node_modules/jquery/dist/jquery.js", dest: "Scripts/jquery/jquery.js"
+                        src: "node_modules/jquery/dist/jquery.js", dest: "Scripts/Lib/jquery/jquery.js"
                     },
                     {
-                        src: "node_modules/jquery/external/sizzle/dist/sizzle.js", dest: "Scripts/jquery/sizzle.js"
+                        src: "node_modules/jquery/external/sizzle/dist/sizzle.js", dest: "Scripts/Lib/jquery/sizzle.js"
                     }
                 ]
             },
             jqueryval: {
                 files: [
                     {
-                        src: "node_modules/jquery-validation/dist/jquery.validate.js", dest: "Scripts/jquery-validation/jquery.validate.js"
+                        src: "node_modules/jquery-validation/dist/jquery.validate.js", dest: "Scripts/Lib/jquery-validation/jquery.validate.js"
                     },
                     {
-                        src: "node_modules/jquery-validation-unobtrusive/dist/jquery.validate.unobtrusive.js", dest: "Scripts/jquery-validation/jquery.validate.unobtrusive.js"
+                        src: "node_modules/jquery-validation-unobtrusive/dist/jquery.validate.unobtrusive.js", dest: "Scripts/Lib/jquery-validation/jquery.validate.unobtrusive.js"
                     }
                 ]
             },
             bootstrap: {
                 files: [
                     {
-                        src: "node_modules/bootstrap/dist/js/bootstrap.js", dest: "Scripts/bootstrap/bootstrap.js"
+                        src: "node_modules/bootstrap/dist/js/bootstrap.js", dest: "Scripts/Lib/bootstrap/bootstrap.js"
                     },
                     {
-                        src: "node_modules/respond.js/src/respond.js", dest: "Scripts/bootstrap/respond.js"
+                        src: "node_modules/respond.js/src/respond.js", dest: "Scripts/Lib/bootstrap/respond.js"
                     }
                 ]
             },
             jqueryui: {
                 files: [
                     {
-                        src: "node_modules/jquery-ui/ui/*.js", dest: "Scripts/jquery-ui/"
+                        src: "node_modules/jquery-ui/ui/*.js", dest: "Scripts/Lib/jquery-ui/"
                     },
                     {
-                        src: "node_modules/jquery-ui/ui/effects/*.js", dest: "Scripts/jquery-ui/"
+                        src: "node_modules/jquery-ui/ui/effects/*.js", dest: "Scripts/Lib/jquery-ui/"
                     },
                     {
-                        src: "node_modules/jquery-ui/ui/widgets/*.js", dest: "Scripts/jquery-ui/"
+                        src: "node_modules/jquery-ui/ui/widgets/*.js", dest: "Scripts/Lib/jquery-ui/"
                     }
                 ]
             },
             offlinejs: {
                 files: [
                     {
-                        src: "node_modules/offline-js/offline.min.js", dest: "Scripts/offline-js/offline.min.js"
+                        src: "node_modules/offline-js/offline.min.js", dest: "Scripts/Lib/offline-js/offline.min.js"
                     }
                 ]
             },
             bootstrapstyle: {
                 files: [
                     {
-                        src: "node_modules/bootstrap/dist/css/bootstrap.min.css", dest: "Stylesheets/bootstrap/bootstrap.min.css"
+                        src: "node_modules/bootstrap/dist/css/bootstrap.min.css", dest: "Stylesheets/Lib/bootstrap/bootstrap.min.css"
                     }
                 ]
             },
             bootstrapglyph: {
                 files: [
                     {
-                        src: "node_modules/bootstrap/dist/fonts/glyphicons-halflings-regular.eot", dest: "Stylesheets/fonts/glyphicons-halflings-regular.eot"
+                        src: "node_modules/bootstrap/dist/fonts/glyphicons-halflings-regular.eot", dest: "Stylesheets/Lib/fonts/glyphicons-halflings-regular.eot"
                     },
                     {
-                        src: "node_modules/bootstrap/dist/fonts/glyphicons-halflings-regular.svg", dest: "Stylesheets/fonts/glyphicons-halflings-regular.svg"
+                        src: "node_modules/bootstrap/dist/fonts/glyphicons-halflings-regular.svg", dest: "Stylesheets/Lib/fonts/glyphicons-halflings-regular.svg"
                     },
                     {
-                        src: "node_modules/bootstrap/dist/fonts/glyphicons-halflings-regular.ttf", dest: "Stylesheets/fonts/glyphicons-halflings-regular.ttf"
+                        src: "node_modules/bootstrap/dist/fonts/glyphicons-halflings-regular.ttf", dest: "Stylesheets/Lib/fonts/glyphicons-halflings-regular.ttf"
                     },
                     {
-                        src: "node_modules/bootstrap/dist/fonts/glyphicons-halflings-regular.woff", dest: "Stylesheets/fonts/glyphicons-halflings-regular.woff"
+                        src: "node_modules/bootstrap/dist/fonts/glyphicons-halflings-regular.woff", dest: "Stylesheets/Lib/fonts/glyphicons-halflings-regular.woff"
                     },
                     {
-                        src: "node_modules/bootstrap/dist/fonts/glyphicons-halflings-regular.woff2", dest: "Stylesheets/fonts/glyphicons-halflings-regular.woff2"
+                        src: "node_modules/bootstrap/dist/fonts/glyphicons-halflings-regular.woff2", dest: "Stylesheets/Lib/fonts/glyphicons-halflings-regular.woff2"
                     }
                 ]
             },
             basestyle: {
                 files: [
                     {
-                        src: "node_modules/jquery-ui/themes/base/core.css", dest: "Stylesheets/base/core.css"
+                        src: "node_modules/jquery-ui/themes/base/core.css", dest: "Stylesheets/Lib/base/core.css"
                     },
                     {
-                        src: "node_modules/jquery-ui/themes/base/resizable.css", dest: "Stylesheets/base/resizable.css"
+                        src: "node_modules/jquery-ui/themes/base/resizable.css", dest: "Stylesheets/Lib/base/resizable.css"
                     },
                     {
-                        src: "node_modules/jquery-ui/themes/base/selectable.css", dest: "Stylesheets/base/selectable.css"
+                        src: "node_modules/jquery-ui/themes/base/selectable.css", dest: "Stylesheets/Lib/base/selectable.css"
                     },
                     {
-                        src: "node_modules/jquery-ui/themes/base/accordion.css", dest: "Stylesheets/base/accordion.css"
+                        src: "node_modules/jquery-ui/themes/base/accordion.css", dest: "Stylesheets/Lib/base/accordion.css"
                     },
                     {
-                        src: "node_modules/jquery-ui/themes/base/autocomplete.css", dest: "Stylesheets/base/autocomplete.css"
+                        src: "node_modules/jquery-ui/themes/base/autocomplete.css", dest: "Stylesheets/Lib/base/autocomplete.css"
                     },
                     {
-                        src: "node_modules/jquery-ui/themes/base/button.css", dest: "Stylesheets/base/button.css"
+                        src: "node_modules/jquery-ui/themes/base/button.css", dest: "Stylesheets/Lib/base/button.css"
                     },
                     {
-                        src: "node_modules/jquery-ui/themes/base/dialog.css", dest: "Stylesheets/base/dialog.css"
+                        src: "node_modules/jquery-ui/themes/base/dialog.css", dest: "Stylesheets/Lib/base/dialog.css"
                     },
                     {
-                        src: "node_modules/jquery-ui/themes/base/slider.css", dest: "Stylesheets/base/slider.css"
+                        src: "node_modules/jquery-ui/themes/base/slider.css", dest: "Stylesheets/Lib/base/slider.css"
                     },
                     {
-                        src: "node_modules/jquery-ui/themes/base/tabs.css", dest: "Stylesheets/base/tabs.css"
+                        src: "node_modules/jquery-ui/themes/base/tabs.css", dest: "Stylesheets/Lib/base/tabs.css"
                     },
                     {
-                        src: "node_modules/jquery-ui/themes/base/datepicker.css", dest: "Stylesheets/base/datepicker.css"
+                        src: "node_modules/jquery-ui/themes/base/datepicker.css", dest: "Stylesheets/Lib/base/datepicker.css"
                     },
                     {
-                        src: "node_modules/jquery-ui/themes/base/progressbar.css", dest: "Stylesheets/base/progressbar.css"
+                        src: "node_modules/jquery-ui/themes/base/progressbar.css", dest: "Stylesheets/Lib/base/progressbar.css"
                     },
                     {
-                        src: "node_modules/jquery-ui/themes/base/theme.css", dest: "Stylesheets/base/theme.css"
+                        src: "node_modules/jquery-ui/themes/base/theme.css", dest: "Stylesheets/Lib/base/theme.css"
                     },
                     {
-                        src: "node_modules/js-autocomplete/auto-complete.css", dest: "Stylesheets/base/auto-complete.css"
+                        src: "node_modules/js-autocomplete/auto-complete.css", dest: "Stylesheets/Lib/base/auto-complete.css"
                     }
                 ]
             },
             offlinestyle: {
                 files: [
                     {
-                        src: "node_modules/offline-js/themes/offline-theme-slide.css", dest: "Stylesheets/offline/offline-theme-slide.css"
+                        src: "node_modules/offline-js/themes/offline-theme-slide.css", dest: "Stylesheets/Lib/offline/offline-theme-slide.css"
                     },
                     {
-                        src: "node_modules/offline-js/themes/offline-language-english.css", dest: "Stylesheets/offline/offline-language-english.css"
+                        src: "node_modules/offline-js/themes/offline-language-english.css", dest: "Stylesheets/Lib/offline/offline-language-english.css"
                     },
                     {
-                        src: "node_modules/offline-js/themes/offline-theme-slide-indicator.css", dest: "Stylesheets/offline/offline-theme-slide-indicator.css"
+                        src: "node_modules/offline-js/themes/offline-theme-slide-indicator.css", dest: "Stylesheets/Lib/offline/offline-theme-slide-indicator.css"
                     },
                     {
-                        src: "node_modules/offline-js/themes/offline-language-english-indicator.css", dest: "Stylesheets/offline/offline-language-english-indicator.css"
+                        src: "node_modules/offline-js/themes/offline-language-english-indicator.css", dest: "Stylesheets/Lib/offline/offline-language-english-indicator.css"
                     }
                 ]
             },
             table: {
                 files: [
                     {
-                        src: "node_modules/floatthead/dist/jquery.floatThead.min.js", dest: "Scripts/table/jquery.floatThead.min.js"
+                        src: "node_modules/floatthead/dist/jquery.floatThead.min.js", dest: "Scripts/Lib/table/jquery.floatThead.min.js"
                     },
                     {
-                        src: "node_modules/tablesorter/dist/js/jquery.tablesorter.min.js", dest: "Scripts/table/jquery.tablesorter.min.js"
+                        src: "node_modules/tablesorter/dist/js/jquery.tablesorter.min.js", dest: "Scripts/Lib/table/jquery.tablesorter.min.js"
                     }
                 ]
             },
             datatable: {
                 files: [
-                    { src: "node_modules/datatables.net/js/jquery.dataTables.min.js", dest: "Scripts/datatable/jquery.dataTables.min.js" },
-                    { src: "node_modules/datatables.net-bs/js/dataTables.bootstrap.min.js", dest: "Scripts/datatable/dataTables.bootstrap.min.js" },
-                    { src: "node_modules/datatables.net-fixedcolumns/js/dataTables.fixedColumns.min.js", dest: "Scripts/datatable/dataTables.fixedColumns.min.js" },
-                    { src: "node_modules/datatables.net-fixedheader/js/dataTables.fixedHeader.min.js", dest: "Scripts/datatable/dataTables.fixedHeader.min.js" }
+                    { src: "node_modules/datatables.net/js/jquery.dataTables.min.js", dest: "Scripts/Lib/datatable/jquery.dataTables.min.js" },
+                    { src: "node_modules/datatables.net-bs/js/dataTables.bootstrap.min.js", dest: "Scripts/Lib/datatable/dataTables.bootstrap.min.js" },
+                    { src: "node_modules/datatables.net-fixedcolumns/js/dataTables.fixedColumns.min.js", dest: "Scripts/Lib/datatable/dataTables.fixedColumns.min.js" },
+                    { src: "node_modules/datatables.net-fixedheader/js/dataTables.fixedHeader.min.js", dest: "Scripts/Lib/datatable/dataTables.fixedHeader.min.js" }
                 ]
             },
             datatablestyle: {
                 files: [
                     {
-                        src: "node_modules/datatables.net-bs/css/dataTables.bootstrap.min.css", dest: "Stylesheets/datatable/dataTables.bootstrap.min.css"
+                        src: "node_modules/datatables.net-bs/css/dataTables.bootstrap.min.css", dest: "Stylesheets/Lib/datatable/dataTables.bootstrap.min.css"
                     },
                     {
-                        src: "node_modules/datatables.net-fixedcolumns-bs/css/fixedColumns.bootstrap.min.css", dest: "Stylesheets/datatable/fixedColumns.bootstrap.min.css"
+                        src: "node_modules/datatables.net-fixedcolumns-bs/css/fixedColumns.bootstrap.min.css", dest: "Stylesheets/Lib/datatable/fixedColumns.bootstrap.min.css"
                     },
                     {
-                        src: "node_modules/datatables.net-fixedheader-bs/css/fixedHeader.bootstrap.min.css", dest: "Stylesheets/datatable/fixedHeader.bootstrap.min.css"
+                        src: "node_modules/datatables.net-fixedheader-bs/css/fixedHeader.bootstrap.min.css", dest: "Stylesheets/Lib/datatable/fixedHeader.bootstrap.min.css"
                     }
                 ]
             },
             fontawesome: {
                 files: [
-                    { src: "node_modules/font-awesome/css/font-awesome.min.css", dest: "Stylesheets/fontawesome/font-awesome.min.css" }
+                    { src: "node_modules/font-awesome/css/font-awesome.min.css", dest: "Stylesheets/Lib/fontawesome/font-awesome.min.css" }
                 ]
             }
 
